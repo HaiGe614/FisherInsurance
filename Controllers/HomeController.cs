e53dcafdb5c0e34c47d239f794611cea230b70cd
Wan Haig <Wan Haig>
2017-02-13 23:04:33 -0500

    Exercise4ofLab3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,12 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+namespace FisherInsurance.controllers{
 public class HomeController : Controller 
 {
     public IActionResult Index() {
     //return Ok("This is the index of the HomeController"); 
-    return View();
+    return View()
+    ;
 } 
-
+}
 }
